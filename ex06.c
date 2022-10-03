15e8d50f79f200cb1c956642983c75fc22498b81
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2022-10-03 14:29:15 +0100

    exercice 6 et 7

---
+++ ex06.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+int main(){
+	char tablo[50];
+	write(1,"Tapez votre input ci-dessous\n",29);
+	scanf("%[^\n]",&tablo);
+	tablo[41] = '\0';
+	write(1,&tablo,50);
+
+}
