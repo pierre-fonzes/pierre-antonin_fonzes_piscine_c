f761815758a02940fdd5ddedfd2d10ea2e6e02d8
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2022-10-03 15:09:19 +0100

    exercice 8

---
+++ j01/ex08.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool o_pow3(int n)
+{
+	if(n==1)
+	{
+		return true;
+	}
+	if(n%3 != 0)
+	{
+		return false;
+	}
+	o_pow3(n/3);
+}
+
+int main()
+{
+	int res = 9;
+	printf("%s\n", o_pow3(res)?"true":"false");
+}
