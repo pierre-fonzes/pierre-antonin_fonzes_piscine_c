7d8b8330f113000b4e5bdec75a12076258a29f85
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2022-10-03 14:55:29 +0100

    correction avec ajout du param size

--- j01/ex07.c
+++ j01/ex07.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 
-long o_arrsum(int *arr)
+long o_arrsum(int *arr, int size)
 {
 	int cpt;
 	int sum = 0;
-	for(cpt = 0;cpt<sizeof(arr);cpt++)
+	for(cpt = 0;cpt<size;cpt++)
 	{
 		sum += arr[cpt];
 	}
