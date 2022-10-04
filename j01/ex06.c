75e5c80d960e3435edff26053e0aebc4d73da90f
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2022-10-04 08:00:46 +0100

    update du 06 avec compil avec flags

--- j01/ex06.c
+++ j01/ex06.c
@@ -4,10 +4,10 @@
 
 int	main(void)
 {
-	char	*tablo[50];
+	char	tablo[50];
 
 	write(1, "Tapez votre input ci-dessous\n", 29);
-	scanf("%[^\n]", &tablo);
+	scanf("%[^\n]", tablo);
 	tablo[41] = '\0';
 	write(1, &tablo, strlen(tablo));
 }
