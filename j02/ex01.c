62cc99a9b33e8e5b06b36ae843fdd21c5c0ea5f0
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2022-10-04 08:45:23 +0100

    fonction affiche avec tests

---
+++ j02/ex01.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void o_print_char(char c)
+{
+	write(1,&c,1);
+}
+
+
+
+void o_print_string(char *str)
+{
+	int i;
+
+	while(str[i] != '\0')
+	{
+		o_print_char(str[i]);
+		i++;
+	}
+	write(1,"\n",1);
+	i = 0;
+
+}
+
+int	main(void)
+{
+	char tablo[10] = "Le c nul";
+	o_print_string(tablo);
+
+	char tab[50] = "LA PENDAséon";
+	o_print_string(tab);
+
+	char tabl[50] = "   123 c'ést nul";
+	o_print_string(tabl);
+}
