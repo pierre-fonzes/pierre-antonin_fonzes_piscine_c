210c995a08c8c8899f9c16bbbae0b2eb82d180a7
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2022-10-07 12:55:02 +0100

    ajout des fonctions dans le main
    Comme je code sur replit j'avais des erreurs sur les includes lorsque je voulais lancer le main, j'ai donc mis toutes mes fonctions dans le programme

--- projet pendu/main.c
+++ projet pendu/main.c
@@ -2,14 +2,101 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 #include "test.h"
 #define TAILLE_MAX 100
-#include "lireCar.h"
-#include "lettreDansMot.h"
-#include "decouvrirLettre.h"
-#include "choisirMot.h"
+//#include "lireCar.h"
+//#include "lettreDansMot.h"
+//#include "decouvrirLettre.h"
+//#include "choisirMot.h"
 
-int main(int argc, char *argv[])
+int nombreAleatoire(int nombreMax)
+{
+    srand(time(NULL));
+    return (rand() % nombreMax);
+}
+
+int piocherMot(char *motPioche)
+{
+    FILE* dico = NULL;
+    int nombreMots = 0, numMotChoisi = 0;
+    int caractereLu = 0;
+    dico = fopen("test.txt", "r");
+
+    // On vérifie si on a réussi à ouvrir le dictionnaire
+    if (dico == NULL) 
+    {
+        printf("\nImpossible de charger le dictionnaire de mots\n");
+        return 0; // On retourne 0 pour indiquer que la fonction a échoué
+    }
+
+    // On compte le nombre de mots dans le fichier
+    //on compte uniquement les \n sinon le compte sera mauvais car \n indique la fin du mot 
+    do
+    {
+        caractereLu = fgetc(dico);
+        if (caractereLu == '\n')
+            nombreMots++;
+    } while(caractereLu != EOF);
+
+    numMotChoisi = nombreAleatoire(nombreMots); // On pioche un mot au hasard
+
+    // On recommence à lire le fichier depuis le début. On s'arrête lorsqu'on est arrivé au bon mot
+    rewind(dico);
+    while (numMotChoisi > 0)
+    {
+        caractereLu = fgetc(dico);
+        if (caractereLu == '\n')
+            numMotChoisi--;
+    }
+
+    //On a placé le curseur au bon endroit 
+    //on a plus qu'a stocker le mot dans motPioche
+    fgets(motPioche, 100, dico);
+
+    // On vire le \n à la fin
+    motPioche[strlen(motPioche) - 1] = '\0';
+    fclose(dico);
+
+    return 1; // Tout s'est bien passé, on retourne 1
+}
+
+
+void decouvrirLettre(char lettre, char* motSecret,int nbLettres,char *affichage){
+  for(int i =0;i<nbLettres;i++){
+    if(lettre == motSecret[i]){
+      affichage[i] = lettre;
+    }
+  }
+  printf("Bravo, Cette lettre fait bien partie du mot\n");
+}
+
+int lettreDansMot(char lettre, char *motSecret){
+  int i = 0;
+  while(motSecret[i] != '\0'){
+    if(lettre == motSecret[i]){
+      return 1;
+    }
+    i++;
+  }
+  return  0;
+}
+
+char lireLettre() 
+{ 
+    char caractere = 0;
+ 
+    caractere = getchar(); // On lit le premier caractère
+    caractere = toupper(caractere); // On met la lettre en majuscule si elle ne l'est pas déjà
+    //afin d'éviter toute erreur de saisie de l'utilisateur car les mots sont en majuscules
+ 
+    // On lit les autres caractères mémorisés un à un jusqu'au \n (pour les effacer) 
+    while (getchar() != '\n') ;
+ 
+    return caractere; // On retourne le premier caractère qu'on a lu 
+}
+
+int main()
 {
   FILE *fd = fopen("test.txt","r");
   if(fd==NULL){
@@ -67,30 +154,6 @@ int main(int argc, char *argv[])
     printf("Le mot Secret était : ");
     printf("%s\n",motSecret);
   }
-
-  /*char chaine[1000000];
-  fgets(chaine,10000000,fd);
-  char chaine1[15];
-  fgets(chaine1,15,fd);
-  //printf("%c\n",chaine1[0]);
-
-  char carActuel;
-  carActuel = fgetc(fd);
-  //printf("%c\n",carActuel);
-  if(strcmp(&carActuel,"#")==0){
-      fseek(fd,0,SEEK_SET);
-      printf("%d\n",1);
-  }
-  else{
-      fseek(fd,0,SEEK_SET);
-       printf("%d\n",0);
-  }
-
-  FILE *fd1 = fopen("sortie.txt","w");
-  char chaine2[100];
-  while(fgets(chaine2,100,fd)!=NULL){//tant que fgets renvoie pas null on écrit tout dans le fichier de sortie
-      fputs(chaine2,fd1);
-  }*/
 
   
   fclose(fd);
